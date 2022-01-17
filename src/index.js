7b644fce79a652cd7f6675e8bc8b2c12ea9edc03
Bruno <bruno.campiol@gmail.com>
2022-01-17 10:44:13 -0300

    adding index src files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'React-dom';
+import React from 'React';
+
+const App = () => {
+ return <h1>React app component 👍</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('appRoot'));
